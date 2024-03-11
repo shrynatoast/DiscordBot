af68fce1dfb62a7397da5922e369dfb9942cada1
shrynatoast <158052567+shrynatoast@users.noreply.github.com>
2024-03-11 16:33:04 +0800

    Update botpolusi.py

--- botpolusi.py
+++ botpolusi.py
@@ -12,35 +12,38 @@ async def on_ready():
 
 
 @bot.command()
-async def halo(ctx):
-    await ctx.send('Halo! ')
-
-
-@bot.command()
 async def bantuan(ctx):
     embed = discord.Embed(
         title="daftar",
         description="Daftar perintah yang tersedia:",
         color=discord.Color.blue()
     )
-    embed.add_field(name="!poin1", value="penjelasan bagaimana cara mengurai sampah menjadi seni", inline=False)
-    embed.add_field(name="!poin2", value="contoh kerajinan seni dari barang bekas", inline=False)
-    embed.add_field(name="!poin3", value="alat dan bahan", inline=False)
-    embed.add_field(name="!poin4", value="langkah langkah", inline=False)
+    embed.add_field(name="!penjelasan", value="tujuan project bot ini", inline=False)
+    embed.add_field(name="!contoh", value="contoh kerajinan seni dari barang bekas", inline=False)
+    embed.add_field(name="!alat", value="", inline=False)
+    embed.add_field(name="!pembuatan", value="", inline=False)
+    embed.add_field(name="Bonus", value="coba katakan 'hai', sambil menyebut namanya ( iroha ) ", inline=False)
 
     await ctx.send(embed=embed)
 
 @bot.command()
-async def poin1(ctx):
-    await ctx.send('Pengurangan Limbah: Salah satu tujuan utama adalah mengurangi jumlah sampah yang masuk ke tempat pembuangan akhir atau berakhir di lingkungan. Dengan mengubah sampah menjadi barang yang berguna atau dekoratif, kita dapat meminimalkan dampak negatifnya terhadap lingkungan.')
-    await ctx.send('Halo! ')
-
-@bot.command()
-async def poin2(ctx):
+async def contoh(ctx):
     with open('image/contoh.jpg', 'rb') as f:
         picture = discord.File(f)
-    await ctx.send('berikut hasil kerajinan barang bekas ')
+    await ctx.send('berikut contoh kerajinan barang bekas ')
     await ctx.send(file=picture)
 
+@bot.command()
+async def penjelasan(ctx):
+    await ctx.send('Pengurangan Limbah: Salah satu tujuan utama adalah mengurangi jumlah sampah yang masuk ke tempat pembuangan akhir atau berakhir di lingkungan. Dengan mengubah sampah menjadi barang yang berguna atau dekoratif, kita dapat meminimalkan dampak negatifnya terhadap lingkungan.')
 
-bot.run('TOKEN_BOT')
+@bot.command()
+async def pembuatan(ctx):
+    await ctx.send('teks')
+
+@bot.command()
+async def alat(ctx):
+    await ctx.send('teks')
+
+
+bot.run('t o k e n')
